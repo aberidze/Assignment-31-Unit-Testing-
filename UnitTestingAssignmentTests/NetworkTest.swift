03901beb1bf6004d5a8e2d3571afc34d412662a3
AnriBeridzee <aberidze02@gmail.com>
2023-12-04 15:12:59 +0400

    Add Network Tests

---
+++ UnitTestingAssignmentTests/NetworkTest.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkTest.swift
+//  UnitTestingAssignmentTests
+//
+//  Created by Macbook Air 13 on 04.12.23.
+//
+
+import XCTest
+@testable import UnitTestingAssignment
+
+final class NetworkTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
